731470ccf143642922b05379dfe36a81fa341e98
Michael Griffin <mdgriffin064@gmail.com>
2017-02-09 14:51:45 +0000

    Added additional form field

--- BuildSys/MainWindow.xaml.cs
+++ BuildSys/MainWindow.xaml.cs
@@ -29,5 +29,18 @@ namespace BuildSys
         {
             MessageBox.Show("Register a new customer");
         }
+
+        private void cmbCounty_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>() {
+                "Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Derry", "Donegal",
+                "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", "Laois",
+                "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly",
+                "Roscommon", "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"
+            };
+
+            cmbCounty.ItemsSource = data;
+        }
+
     }
 }
