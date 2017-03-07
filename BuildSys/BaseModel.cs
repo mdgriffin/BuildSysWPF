7c487ab8b5bd6e62cdb821f40129ea7a624779ed
Michael Griffin <mdgriffin064@gmail.com>
2017-03-07 11:27:31 +0000

    Added item to menu

--- BuildSys/BaseModel.cs
+++ BuildSys/BaseModel.cs
@@ -10,8 +10,8 @@ namespace BuildSys
 {
     public class BaseModel : INotifyPropertyChanged
     {
-        //public const string CONNECTION_STRING = "Data Source = oracle/orcl; User Id = T00119683; Password = w8qqptj9;";
-        public const string CONNECTION_STRING = "Data Source = Localhost; User Id = T00119683; Password = w8qqptj9;";
+        public const string CONNECTION_STRING = "Data Source = oracle/orcl; User Id = T00119683; Password = w8qqptj9;";
+        //public const string CONNECTION_STRING = "Data Source = Localhost; User Id = T00119683; Password = w8qqptj9;";
 
         public event PropertyChangedEventHandler PropertyChanged;
 
