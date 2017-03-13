0fba33e2da83078c50d05d6047b00d1e7d7f6a59
Michael Griffin <mdgriffin064@gmail.com>
2017-03-13 12:58:22 +0000

    Moved Counties to Static Resource

--- BuildSys/CustomerForm.xaml.cs
+++ BuildSys/CustomerForm.xaml.cs
@@ -14,12 +14,14 @@ namespace BuildSys
         {
             InitializeComponent();
 
+            /*
             Counties = new List<string>() {
                 "Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Derry", "Donegal",
                 "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", "Laois",
                 "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly",
                 "Roscommon", "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"
             };
+            */
 
             Titles = new List<string>() { "Mr", "Mrs", "Ms", "Miss", "Mx" };
 
