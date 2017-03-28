376d224a65921bcf53e5e462d665000be81cdc09
Michael Griffin <mdgriffin064@gmail.com>
2017-03-28 12:53:31 +0100

    Adding selected material to add material inputs

--- BuildSys/ViewModels/QuoteFormViewModel.cs
+++ BuildSys/ViewModels/QuoteFormViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BuildSys.Models;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace BuildSys.ViewModels
 {
@@ -39,6 +40,42 @@ namespace BuildSys.ViewModels
         public QuoteModel quote { get; set; }
         public QuoteMaterialModel material { get; set; }
 
+        public MaterialModel _selectedMaterial;
+        public MaterialModel selectedMaterial {
+            get
+            {
+                return _selectedMaterial;
+            }
+            set
+            {
+                if (value != _selectedMaterial)
+                {
+                    _selectedMaterial = value;
+                    NotifyPropertyChanged("selectedMaterial");
+                }
+            }
+        }
+
         public ObservableCollection<MaterialModel> materialList { get; set; }
+
+        public ICommand addMaterialToQuoteCmd
+        {
+            get
+            {
+                return new RelayCommand(materialId => addMaterial((int) materialId), param => canAddMaterial());
+            }
+        }
+
+
+        public void addMaterial (int materialId)
+        {
+            selectedMaterial = MaterialModel.getMaterial(materialId);
+        }
+
+
+        public Boolean canAddMaterial ()
+        {
+            return true;
+        }
     }
 }
