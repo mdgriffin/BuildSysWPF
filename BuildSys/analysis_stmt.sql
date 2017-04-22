2ae5004f4a7349fdbe74b18ae8f608799d111c6d
Michael Griffin <mdgriffin064@gmail.com>
2017-04-21 20:53:14 -0700

    Added sql statement files

---
+++ BuildSys/analysis_stmt.sql
@@ -0,0 +1,86 @@
+﻿/**
+ * Material Analysis 
+ **/
+
+-- All Material
+SELECT * FROM Materials;
+
+-- Number of Materials
+SELECT COUNT(material_id) FROM Materials;
+
+-- Average Material Cost Growth??
+--SELECT * FROM Quote_Materials;
+
+-- Materials Created By Month
+SELECT AVG(COUNT(M.MATERIAL_ID)) AS Num_Materials_Per_Month FROM Quote_Materials M, Quotes Q WHERE M.Quote_id = Q.Quote_id GROUP BY EXTRACT(MONTH FROM DATE_ISSUED);
+
+-- Average Number of Materials Per Quote
+SELECT AVG(COUNT(MATERIAL_ID)) AS Average_Materials_Per_Quote FROM Quote_Materials GROUP BY Quote_Id;
+
+-- Number of Non-Service Materials
+SELECT count(material_id) FROM Materials Where Is_Service = 0;
+
+-- Number of Service
+SELECT count(material_id) FROM Materials Where Is_Service = 1;
+
+-- Most expensive material
+SELECT * FROM Materials WHERE Price_Per_Unit = (SELECT MAX(Price_Per_Unit) FROM Materials);
+
+-- Average Material Cost
+SELECT ROUND(AVG(Price_Per_Unit), 2) FROM Materials;
+
+-- Divide by Price
+select m.price_range as Material_Price_Range, count(*) as number_of_occurences
+from (
+  select case  
+    when price_per_unit between  0 and 10 then  '0 - 10'
+    when price_per_unit between 10 and 20 then '10 - 20'
+    when price_per_unit between 20 and 30 then '20 - 30'
+    when price_per_unit between 30 and 40 then '30 - 40'
+    when price_per_unit between 40 and 50 then '40 - 50'
+    when price_per_unit between 50 and 60 then '50 - 60'
+    when price_per_unit between 60 and 70 then '60 - 70'
+    when price_per_unit between 70 and 80 then '70 - 80'
+    when price_per_unit between 80 and 80 then '80 - 90'
+    when price_per_unit between 90 and 100 then '90 - 100'
+    else '100+' end as price_range
+  from Materials) m
+group by m.price_range;
+
+/**
+ * Quote Analysis 
+ **/
+
+-- All Quotes
+SELECT * FROM Quotes;
+
+-- Number of Quotes
+SELECT Count(Quote_Id) FROM Quotes;
+
+-- Average Quote Value
+SELECT AVG(Subtotal + Vat) FROM Quotes;
+
+-- Total Quotes Issued
+SELECT SUM(Subtotal + Vat) FROM Quotes;
+
+-- Largest Quote Issued
+SELECT SUM(Subtotal + Vat) FROM Quotes;
+
+-- Get number of Quotes issued by month
+SELECT  EXTRACT(MONTH FROM DATE_ISSUED), COUNT(quote_id) AS Quotes_Per_Month  FROM Quotes Q GROUP BY EXTRACT(MONTH FROM DATE_ISSUED) ORDER BY  EXTRACT(MONTH FROM DATE_ISSUED);
+
+-- Number of Quotes issued by year, Limit to last 5 years
+SELECT  EXTRACT(YEAR FROM DATE_ISSUED), COUNT(quote_id) AS Quotes_Per_Year  FROM Quotes Q WHERE ROWNUM < 6 GROUP BY EXTRACT(YEAR FROM DATE_ISSUED) ORDER BY  EXTRACT(YEAR FROM DATE_ISSUED);
+
+/**
+ * Customer Analysis 
+ **/
+
+-- All Customers
+SELECT * FROM Customers;
+
+-- Number of Customers
+SELECT COUNT(customer_id) FROM Customers;
+
+-- Best Customer
+--SELECT * FROM Customers WHERE customer_id IN (SELECT customer_id from Quotes GROUP BY customer_id ORDER BY vat);
---
+++ BuildSys/dummy_data.sql
@@ -0,0 +1,32 @@
+﻿
+
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(5, 'DCE ltd', 'Mr', 'Joe', 'Reilly', '12 Watercress Street', 'Castleisland', 'Kerry', '066123456', 'joe@example.com', 'IE293847383', 'B', 'A');
+    
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(6, 'Fitzgibbon Sisters ltd', 'Mrs', 'Emma', 'Fitzgibbon', '23 Strand hill', 'Dooks Glenbeigh', 'Kerry', '063123456', 'emma@example.com', 'IE34545534', 'B', 'A');
+    
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(7, 'Appleton Interiors', 'Mr', 'Joe', 'Reilly', '12 Watercress Street', 'Castleisland', 'Kerry', '066123456', 'joe@example.com', 'IE293847383', 'B', 'A');
+    
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(8, 'John Twomey Construction Ltd.', 'Mr', 'John', 'Towomey', 'Orchard House Main Street', 'Castlegregory', 'Kerry', '0666543311', 'twomey.construction@example.com', 'IE567474357', 'B', 'A');
+    
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(9, 'The Sports Bar', 'Mr', 'Harry', 'Counihan', '43 Castleford Avenue', 'Newcastle West', 'Limerick', '0962384839', 'harry54@example.com', 'IE5676577663', 'B', 'A');
+
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(10, 'The Grand Hotel', 'Mrs', 'Alana', 'Turner', 'High Street', 'Kenmare', 'Kerry', '06134534545', 'alana@example.com', 'IE4564564567', 'B', 'A');
+    
+INSERT INTO Customers
+    (customer_id, company_name, title, firstname, surname, street, town, county, telephone, email, vat_no, account_type, status)
+    VALUES(11, 'Templeton Nursing Home', 'Ms', 'Geraldine', 'Phillips', 'Tralee Road', 'Milltown', 'Kerry', '0665612789', 'templeton@example.com', 'IE456456654', 'B', 'A');
+    
+    
+COMMIT;
