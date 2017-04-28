82b7966fbb58d1077c5005fc9b5fa3e464ab3c8c
Michael Griffin <mdgriffin064@gmail.com>
2017-04-28 14:16:20 +0100

    Added more comments to customer form and manage

--- BuildSys/ViewModels/CustomerFormViewModel.cs
+++ BuildSys/ViewModels/CustomerFormViewModel.cs
@@ -9,28 +9,32 @@ namespace BuildSys.ViewModels
     
     public class CustomerFormViewModel: BaseViewModel
     {
-       //MainViewModel parent;
         
-        
-        /// <summary>
-        /// Initializes a new instance of the CustomerFormViewModel class.
-        /// </summary>
+        // Default constructor for registering a new customer
         public CustomerFormViewModel(BaseViewModel parent)
         {
             customer = new CustomerModel();
             customer.PropertyChanged += onCustomerPropertyChanged;
 
+            // The business inputs are not visible by default
             showBusinessInputs = "Collapsed";
 
             btnText = "Register Customer";
             this.parent = parent;
         }
 
+        // Default constructor for updating the customer
         public CustomerFormViewModel(BaseViewModel parent, int customerId)
         {
+            this.parent = parent;
+
+            // Get the customer being updated
             customer = CustomerModel.getCustomer(customerId);
+
+            // Register another event handler for checking when the customer property is changed
             customer.PropertyChanged += onCustomerPropertyChanged;
 
+            // Set the business inputs to visible or collapsed based on account type
             if (customer.accountType == 'B')
             {
                 showBusinessInputs = "Visible";
@@ -39,13 +43,14 @@ namespace BuildSys.ViewModels
                 showBusinessInputs = "Collapsed";
             }
 
+            // Since we are updating, set the correct button text
             btnText = "Update Customer";
-            this.parent = parent;
         }
 
+        // Event handler called when a propert is changed
+        // Used to show and hide the business inputs
         public void onCustomerPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-
             if (e.PropertyName.Equals("accountType"))
             {
                 if (customer.accountType == 'B')
@@ -59,10 +64,12 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Initializing the public variables accessed in the view
         public CustomerModel customer { get; set; }
         public String showBusinessInputs { get; set; }
         public String btnText { get; set; }
 
+        // Binding a command to handle clicks on the save customer button
         public ICommand saveCustomerCommand
         {
             get
@@ -71,12 +78,16 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Saves or updates the customer
         public void saveCustomer ()
         {
+            // Forces all properties to be validated
             customer.validateAllProps();
 
+            // Check if the form has errors
             if (!customer.HasErrors)
             {
+                // Check are we updating or registering
                 if (btnText.Equals("Update Customer"))
                 {
                     // Update the Customer's record
@@ -84,10 +95,12 @@ namespace BuildSys.ViewModels
 
                     MessageBox.Show("Customer Updated Successfully");
 
+                    // Navigate to the same view model, in effect resetting the form
                     navigateTo(new CustomerManageViewModel(parent));
                 }
                 else
                 {
+                    // Insert this customer model
                     customer.insertCustomer();
 
                     MessageBox.Show("Sucessfully Register a new customer");
@@ -98,6 +111,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Method used with the saveCustomerCommand to enable and diabled the button
         public Boolean canSaveCustomer ()
         {
             return !customer.HasErrors;
--- BuildSys/ViewModels/CustomerManageViewModel.cs
+++ BuildSys/ViewModels/CustomerManageViewModel.cs
@@ -16,7 +16,9 @@ namespace BuildSys.ViewModels
         {
             this.parent = parent;
 
+            // Gets a list of all active customers
             CustomerList = CustomerModel.getCustomerList();
+            // Gets a list of all deleted customers
             DeletedCustomerList = CustomerModel.getDeletedCustomerList();
 
             // Keep a copy of the CustomerList so that we can restore the list after filtering
@@ -34,11 +36,13 @@ namespace BuildSys.ViewModels
                 if (value != _customerFilter)
                 {
                     _customerFilter = value;
+                    // When changes filter customers
                     filterCustomers();
                 }
             }
         }
 
+        // Public variables accessible in the view
         public ObservableCollection<CustomerModel> DeletedCustomerList { get; set; }
 
         private ObservableCollection<CustomerModel> originalCustomerList;
@@ -55,6 +59,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Command called when the edit customer button is clicked
         public ICommand editCustomerCmd
         {
             get
@@ -63,6 +68,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Command called when delete customer button is clicked
         public ICommand deleteCustomerCmd
         {
             get
@@ -71,6 +77,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Handles the create quote button
         public ICommand createQuoteCmd
         {
             get
@@ -79,6 +86,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // handles the view customers button click
         public ICommand viewCustomerQuoteCmd
         {
             get
@@ -87,6 +95,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Called when the restore customer button is clicked
         public ICommand restoreCustomerCmd
         {
             get
@@ -95,48 +104,65 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Nagiviate to the edit customer view
         public void editCustomer (int customerId)
         {
             navigateTo(new CustomerFormViewModel(parent, customerId));
         }
 
+        // Deletes the specified customer
         public void deleteCustomer(int customerId)
         {
             CustomerModel.deleteCustomer(customerId);
 
             CustomerModel deletedCustomer = CustomerList.Where(cust => cust.customerId == customerId).First();
 
+            // Remove from the specified customer from the customer list
             CustomerList.Remove(deletedCustomer);
+            // Also remove from the original customer list
             originalCustomerList.Remove(deletedCustomer);
 
+            // Add to the deleted customer list
             DeletedCustomerList.Add(deletedCustomer);
         }
 
+        // Restores a customer back 
         public void restoreCustomer(int customerId)
         {
+            // Changes the customer status back to active
             CustomerModel.restoreCustomer(customerId);
+            // Get the model of the restored customer
             CustomerModel restoredCustomer = DeletedCustomerList.Where(cust => cust.customerId == customerId).First();
 
+            // Add the model to the active customer list
             CustomerList.Add(restoredCustomer);
             originalCustomerList.Add(restoredCustomer);
 
+            // Remove from the deleted customer list
             DeletedCustomerList.Remove(restoredCustomer);
 
+            // Re-filter the customers
             filterCustomers();
         }
 
+        // navigate to the specifies customer
         public void viewCustomerQuotes(int customerId)
         {
             navigateTo(new CustomerQuoteManageViewModel(parent, customerId));
         }
 
+        // Filters the customer list based on the filter text input
         public void filterCustomers ()
         {
+            // Start the filtering from the full customer list
             CustomerList = new ObservableCollection<CustomerModel>(originalCustomerList);
+            // Create regex to match the customer
             Regex matchName = new Regex(@"^" + customerFilter + @".+", RegexOptions.IgnoreCase);
 
+            // Only filter is text has been entered
             if (customerFilter.Length > 0)
             {
+                // Uses a lambda to filter down customers, check if firstname, surname or company name matches
                 CustomerList.Where(cust => !matchName.IsMatch(cust.firstname) && !matchName.IsMatch(cust.surname) && !matchName.IsMatch(cust.companyName))
                     .ToList()
                     .All(i => CustomerList.Remove(i));
