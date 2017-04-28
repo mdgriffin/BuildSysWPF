1c7fa5f0c971d5581dd365966e4e68192c89cb7d
Michael Griffin <mdgriffin064@gmail.com>
2017-04-28 10:26:52 +0100

    Added comments to the base view model

--- BuildSys/ViewModels/BaseViewModel.cs
+++ BuildSys/ViewModels/BaseViewModel.cs
@@ -6,11 +6,18 @@ using System.Windows.Input;
 
 namespace BuildSys.ViewModels
 {
+    /*
+     * Base View Model from which all view models are extended
+     */
+
     public abstract class BaseViewModel: ObservableObject
     {
+        // A reference to the parent of the view model
         protected BaseViewModel parent;
+        // A stack to store the navigation through the app and allow traversing back
         private static Stack<BaseViewModel> NavigationStack = new Stack<BaseViewModel>(5);
 
+        // The current viewModel that is disaplayed to the user
         private BaseViewModel _ViewModel;
         public BaseViewModel ViewModel
         {
@@ -25,6 +32,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Command that is called when the back button is pressed
         public ICommand backCmd
         {
             get
@@ -33,13 +41,14 @@ namespace BuildSys.ViewModels
             }
         }
 
-
+        // Method to nagivate to a specified view model
         public void navigateTo (BaseViewModel vm)
         {
             NavigationStack.Push(vm);
             parent.ViewModel = vm;
         }
 
+        // Pops from the the stack, navigating to the previous view model
         public void navigateBack ()
         {
             if (canNavigateBack())
@@ -49,6 +58,7 @@ namespace BuildSys.ViewModels
             }
         }
 
+        // Check to see if the back button should be enabled
         public Boolean canNavigateBack()
         {
             return NavigationStack.Count > 1;
